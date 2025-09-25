201ed198d004a532fda00b2bb9a143faad6339e7
Ziqing Yan <yanziqing00@gmail.com>
2025-09-25 19:03:41 +1000

    Add leaflet/dist/leaflet.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "antd/dist/reset.css";
 import "./index.css";
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
